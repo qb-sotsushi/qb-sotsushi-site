d3f2631210678302a98707c9b35c7ff6fe04df8e
qb-sotsushi <311097460+qb-sotsushi@users.noreply.github.com>
2026-07-31 22:03:57 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,94 @@
 import { auth } from "./firebase.js";
 
 import {
-    onAuthStateChanged
+    onAuthStateChanged,
+    signOut
 }
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+
+
+// ログイン状態確認
+
 onAuthStateChanged(auth, function(user){
+
 
     if(!user){
 
+
         location.href = "ログイン.html";
+
+        return;
+
 
     }
 
+
+
+    // ログイン済みならログアウトボタン作成
+
+    createLogoutButton();
+
+
 });
+
+
+
+
+
+// ログアウトボタン作成
+
+function createLogoutButton(){
+
+
+    // すでに存在する場合は作らない
+
+    if(document.getElementById("logoutButton")){
+
+        return;
+
+    }
+
+
+
+    let button = document.createElement("button");
+
+
+    button.id = "logoutButton";
+
+
+    button.innerHTML = "ログアウト";
+
+
+
+    button.onclick = function(){
+
+
+        signOut(auth)
+
+        .then(function(){
+
+
+            location.href = "ログイン.html";
+
+
+        })
+
+
+        .catch(function(error){
+
+
+            console.log(error);
+
+
+        });
+
+
+    };
+
+
+
+    document.body.appendChild(button);
+
+
+}
