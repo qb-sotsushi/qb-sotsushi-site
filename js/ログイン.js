9b87485459cb95930ab8d316f9caf4f25a51feeb
qb-sotsushi <311097460+qb-sotsushi@users.noreply.github.com>
2026-07-31 21:34:56 +0900

    Update ログイン.js

--- js/ログイン.js
+++ js/ログイン.js
@@ -1,41 +1,17 @@
-// Firebase読み込み
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+// Firebase認証読み込み
+import { auth } from "./firebase.js";
 
-import { 
-getAuth,
-signInWithEmailAndPassword
+
+import {
+    signInWithEmailAndPassword
 }
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
 
 
-// Firebase設定
-const firebaseConfig = {
-
-  apiKey: "AIzaSyClRm-_5xVTklFMyO-BJEj7L5m6QD3IAmk",
-  authDomain: "qb-sotsushi-online.firebaseapp.com",
-  projectId: "qb-sotsushi-online",
-  storageBucket: "qb-sotsushi-online.firebasestorage.app",
-  messagingSenderId: "956667713960",
-  appId: "1:956667713960:web:a8e17f78ca12b7eb28e59d",
-  measurementId: "G-5F28NCSWSN"
-
-};
-
-
-
-// Firebase開始
-const app = initializeApp(firebaseConfig);
-
-
-// 認証
-const auth = getAuth(app);
-
-
-
 
 // ログインボタン
+
 document
 .getElementById("loginButton")
 .addEventListener("click", function(){
@@ -46,8 +22,10 @@ document
     document.getElementById("email").value;
 
 
+
     const password =
     document.getElementById("password").value;
+
 
 
 
@@ -58,20 +36,26 @@ document
         password
     )
 
+
     .then(function(){
 
 
         // ログイン成功
-        location.href = "QB卒試online.html";
+
+        location.href =
+        "QB卒試online.html";
 
 
     })
 
 
+
     .catch(function(error){
 
 
-        document.getElementById("error").innerHTML =
+        document
+        .getElementById("error")
+        .innerHTML =
         "メールアドレスまたはパスワードが違います";
 
 
